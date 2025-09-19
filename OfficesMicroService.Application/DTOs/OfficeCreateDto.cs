d60e8ba468641b428e6d95821722536cc0c196c9
alenoktee <elapr943@gmail.com>
2025-09-19 15:15:45 +0300

    added CreateOfficeDto

--- OfficesMicroService.Application/DTOs/OfficeCreateDto.cs
+++ OfficesMicroService.Application/DTOs/OfficeCreateDto.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using Microsoft.OpenApi.MicrosoftExtensions;
+using System.ComponentModel.DataAnnotations;
+
+namespace OfficesMicroService.Application.DTOs;
+
+public class OfficeCreateDto
+{
+    public string? PhotoId { get; set; }
+
+    [Required(ErrorMessage = "Please, enter the office's city")]
+    public string City { get; set; }
+
+    [Required(ErrorMessage = "Please, enter the office’s street")]
+    public string Street { get; set; }
+
+    [Required(ErrorMessage = "Please, enter the office’s house number")]
+    public string HouseNumber { get; set; }
+
+    [Required(ErrorMessage = "Please, enter the office’s number")]
+    public string OfficeNumber { get; set; }
+
+    [Required(ErrorMessage = "Please, enter the phone number")]
+    public string RegistryPhoneNumber { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
